1f09dce84afab39c45eb312d63267695f0cf3c5e
Anthony Martinez <1234567anthony1234567@gmail.com>
2023-09-14 19:06:49 -0700

    Bar Chart Inclusion

--- src/ui/src/MetricsVisualization.js
+++ src/ui/src/MetricsVisualization.js
@@ -1,17 +1,26 @@
 import * as React from "react";
-import { styled } from '@mui/material/styles';
-import Stack from '@mui/material/Stack';
+import { styled } from "@mui/material/styles";
+import Stack from "@mui/material/Stack";
+import { BarChart } from "@mui/x-charts/BarChart";
 
-const Div = styled('div')(({ theme }) => ({
+const Div = styled("div")(({ theme }) => ({
   ...theme.typography.subtitle1,
   backgroundColor: theme.palette.background.paper,
   padding: theme.spacing(1),
 }));
 
+const valueFormatter = (value) => `${value}s`;
+
 export default function SortedArray({ metrics }) {
   return (
     <Stack direction="column">
       <Div>{"Metrics Visualization:"}</Div>
+      <BarChart
+        series={[{ data: Object.values(metrics), label: "Algorithm Efficiency", valueFormatter }]}
+        xAxis={[{ data: Object.keys(metrics), scaleType: "band" }]}
+        width={500}
+        height={500}
+      />
     </Stack>
   );
 }
