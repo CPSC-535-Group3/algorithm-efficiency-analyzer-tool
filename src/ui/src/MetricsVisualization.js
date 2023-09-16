7572e2c88a240126ba845c19edfecab48d748df5
Anthony Martinez <1234567anthony1234567@gmail.com>
2023-09-15 20:36:24 -0700

    Updated metrics visualization

--- src/ui/src/MetricsVisualization.js
+++ src/ui/src/MetricsVisualization.js
@@ -1,7 +1,9 @@
-import * as React from "react";
+import React from "react";
+import { Bar } from "react-chartjs-2";
 import { styled } from "@mui/material/styles";
+import Chart from "chart.js/auto";
 import Stack from "@mui/material/Stack";
-import { BarChart } from "@mui/x-charts/BarChart";
+
 
 const Div = styled("div")(({ theme }) => ({
   ...theme.typography.subtitle1,
@@ -9,24 +11,68 @@ const Div = styled("div")(({ theme }) => ({
   padding: theme.spacing(1),
 }));
 
-const valueFormatter = (value) => `${value}s`;
+const SortedArray = ({ metrics }) => {
+  const colorPalette = [
+    "#0000FF", "#FFFF00", "#800080", "#00FF00",
+    "#FF0000", "#FFA500", "#FF69B4", "#008080"
+  ];
 
-export default function SortedArray({ metrics }) {
+  const algorithmNames = Object.keys(metrics);
+  const efficiencyValues = Object.values(metrics);
+
+  const barColors = colorPalette.slice(0, algorithmNames.length);
+
+  const data = {
+    labels: algorithmNames,
+    datasets: [
+      {
+        label: "Efficiency",
+        data: efficiencyValues,
+        backgroundColor: barColors,
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            const label = context.dataset.label || "";
+            const value = context.parsed.y;
+            return `${label}: ${value}s`;
+          },
+        },
+      },
+    },
+    scales: {
+      x: {
+        type: "category",
+        title: {
+          display: true,
+          text: "Algorithms",
+        },
+      },
+      y: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "Efficiency",
+        },
+      },
+    },
+  };
+
+
   return (
     <Stack direction="column">
       <Div>{"Metrics Visualization:"}</Div>
-      <BarChart
-        series={[
-          {
-            data: Object.values(metrics),
-            label: "Algorithm Efficiency",
-            valueFormatter,
-          },
-        ]}
-        xAxis={[{ data: Object.keys(metrics), scaleType: "band" }]}
-        width={500}
-        height={500}
-      />
+      <Bar data={data} options={options} />
     </Stack>
   );
-}
+};
+
+export default SortedArray;
